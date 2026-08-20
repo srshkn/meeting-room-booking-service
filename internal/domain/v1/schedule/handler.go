b107d205c6b5d1486a6fecabfd7b61b70a0e36c4
srshkn <snuqlstyas@gmail.com>
2026-08-21 02:16:06 +0500

    add schedule handler

---
+++ internal/domain/v1/schedule/handler.go
@@ -0,0 +1,14 @@
+package schedule
+
+import (
+	"context"
+
+	v1GenAPI "mrb-service/internal/generated/v1"
+)
+
+type ScheduleHandler struct{}
+
+func (h *ScheduleHandler) PostRoomsRoomIdScheduleCreate(
+	ctx context.Context,
+	request v1GenAPI.PostRoomsRoomIdScheduleCreateRequestObject,
+) (v1GenAPI.PostRoomsRoomIdScheduleCreateResponseObject, error)
