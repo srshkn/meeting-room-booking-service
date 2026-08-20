ad3c0c05690f4499a76258068a2cebc04d1bac69
srshkn <snuqlstyas@gmail.com>
2026-08-21 02:15:42 +0500

    add room handler

---
+++ internal/domain/v1/room/handler.go
@@ -0,0 +1,19 @@
+package room
+
+import (
+	"context"
+
+	v1GenAPI "mrb-service/internal/generated/v1"
+)
+
+type RoomHandler struct{}
+
+func (h *RoomHandler) PostRoomsCreate(
+	ctx context.Context,
+	request v1GenAPI.PostRoomsCreateRequestObject,
+) (v1GenAPI.PostRoomsCreateResponseObject, error)
+
+func (h *RoomHandler) GetRoomsList(
+	ctx context.Context,
+	request v1GenAPI.GetRoomsListRequestObject,
+) (v1GenAPI.GetRoomsListResponseObject, error)
