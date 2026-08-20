54469f5a410648e26a8ff9d550e18ff34ad311c6
srshkn <snuqlstyas@gmail.com>
2026-08-21 02:16:20 +0500

    add slot handler

---
+++ internal/domain/v1/slot/handler.go
@@ -0,0 +1,14 @@
+package slot
+
+import (
+	"context"
+
+	v1GenAPI "mrb-service/internal/generated/v1"
+)
+
+type SlotHandler struct{}
+
+func (h *SlotHandler) GetRoomsRoomIdSlotsList(
+	ctx context.Context,
+	request v1GenAPI.GetRoomsRoomIdSlotsListRequestObject,
+) (v1GenAPI.GetRoomsRoomIdSlotsListResponseObject, error)
