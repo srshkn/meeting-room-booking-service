234a2cdba32d43eb998cf9e1f16d8e0a176dd665
srshkn <snuqlstyas@gmail.com>
2026-08-22 00:21:05 +0500

    add func New of handler

--- internal/http/handler.go
+++ internal/http/handler.go
@@ -3,6 +3,7 @@ package http
 import (
 	"mrb-service/internal/domain/v1/auth"
 	"mrb-service/internal/domain/v1/booking"
+	"mrb-service/internal/domain/v1/meta"
 	"mrb-service/internal/domain/v1/room"
 	"mrb-service/internal/domain/v1/schedule"
 	"mrb-service/internal/domain/v1/slot"
@@ -11,6 +12,7 @@ import (
 )
 
 type Handler struct {
+	*meta.MetaHandler
 	*user.UserHandler
 	*auth.AuthHandler
 	*room.RoomHandler
@@ -19,8 +21,24 @@ type Handler struct {
 	*booking.BookingHandler
 }
 
-func New() *Handler {
-	return &Handler{}
+func New(
+	metaHandler *meta.MetaHandler,
+	userHandler *user.UserHandler,
+	authHandler *auth.AuthHandler,
+	roomHandler *room.RoomHandler,
+	scheduleHandler *schedule.ScheduleHandler,
+	slotHandler *slot.SlotHandler,
+	bookingHandler *booking.BookingHandler,
+) *Handler {
+	return &Handler{
+		MetaHandler:     metaHandler,
+		UserHandler:     userHandler,
+		AuthHandler:     authHandler,
+		RoomHandler:     roomHandler,
+		ScheduleHandler: scheduleHandler,
+		SlotHandler:     slotHandler,
+		BookingHandler:  bookingHandler,
+	}
 }
 
 var _ v1GenAPI.StrictServerInterface = (*Handler)(nil)
